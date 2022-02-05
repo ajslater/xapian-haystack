05895dd658d9fb84591b6721a3701e1d577f9abd
AJ Slater <aj@slater.net>
2022-02-05 13:39:43 -0800

    try fix err

--- tests/xapian_tests/tests/test_management_commands.py
+++ tests/xapian_tests/tests/test_management_commands.py
@@ -14,7 +14,7 @@ from .test_backend import HaystackBackendTestCase
 
 class ManagementCommandTestCase(HaystackBackendTestCase, TestCase):
 
-    NUM_BLOG_ENTRIES = 20
+    NUM_BLOG_ENTRIES = 50
 
     def get_index(self):
         return BlogSearchIndex()
@@ -81,7 +81,7 @@ class ManagementCommandTestCase(HaystackBackendTestCase, TestCase):
             workers=2,
             batchsize=5,
         )
-        err = sys.stderr.getValue()
+        err = sys.stderr.getvalue()
         print("ERR")
         print(err)
         self.assertNotIn("xapian.DatabaseLockError", err)
