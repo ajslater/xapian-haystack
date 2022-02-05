83c0a62a44b978ab03067e5e8ab33c4dfd64e45d
AJ Slater <aj@slater.net>
2022-02-05 13:41:34 -0800

    back to more workers

--- tests/xapian_tests/tests/test_management_commands.py
+++ tests/xapian_tests/tests/test_management_commands.py
@@ -14,7 +14,7 @@ from .test_backend import HaystackBackendTestCase
 
 class ManagementCommandTestCase(HaystackBackendTestCase, TestCase):
 
-    NUM_BLOG_ENTRIES = 50
+    NUM_BLOG_ENTRIES = 100
 
     def get_index(self):
         return BlogSearchIndex()
@@ -78,8 +78,8 @@ class ManagementCommandTestCase(HaystackBackendTestCase, TestCase):
         call_command(
             "update_index",
             verbosity=2,
-            workers=2,
-            batchsize=5,
+            workers=10,
+            batchsize=10,
         )
         err = sys.stderr.getvalue()
         print("ERR")
