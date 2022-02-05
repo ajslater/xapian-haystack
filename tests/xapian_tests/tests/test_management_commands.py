7b2de594821c5d0b84afddd575f71cc400ec495d
AJ Slater <aj@slater.net>
2022-02-05 13:58:46 -0800

    try with 30

--- tests/xapian_tests/tests/test_management_commands.py
+++ tests/xapian_tests/tests/test_management_commands.py
@@ -14,7 +14,7 @@ from .test_backend import HaystackBackendTestCase
 
 class ManagementCommandTestCase(HaystackBackendTestCase, TestCase):
 
-    NUM_BLOG_ENTRIES = 60
+    NUM_BLOG_ENTRIES = 30
 
     def get_index(self):
         return BlogSearchIndex()
